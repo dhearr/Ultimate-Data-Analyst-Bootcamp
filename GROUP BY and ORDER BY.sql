564b580e3ece2ccfb52f7be7de36e43ad1075cf6
dhearr <dhearr27@gmail.com>
2025-09-09 13:38:31 +0700

    - GROUP BY - ORDER BY

---
+++ GROUP BY and ORDER BY.sql
@@ -0,0 +1,19 @@
+-- GROUP BY --
+
+SELECT *
+FROM employee_demographics;
+
+SELECT gender
+FROM employee_demographics
+GROUP BY gender;
+
+SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender;
+
+-- ORDER BY --
+
+SELECT *
+FROM employee_demographics
+ORDER BY gender, age;
+
