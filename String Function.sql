b35a7756dda7bece0bb1c154c8f2d3d21f2db636
dhearr <dhearr27@gmail.com>
2025-09-11 16:04:27 +0700

    String Function

---
+++ String Function.sql
@@ -0,0 +1,40 @@
+-- String Function --
+
+SELECT first_name, LENGTH(first_name) AS name_length
+FROM employee_demographics
+ORDER BY name_length
+;
+
+SELECT first_name, UPPER(first_name)
+FROM employee_demographics
+;
+
+SELECT first_name, LOWER(first_name)
+FROM employee_demographics
+;
+
+SELECT TRIM('     sky     ') AS name;
+SELECT LTRIM('     sky     ') AS name;
+SELECT RTRIM('     sky     ') AS name;
+
+SELECT first_name,
+LEFT(first_name, 4),
+RIGHT(first_name, 4),
+SUBSTRING(first_name, 3, 2),
+birth_date,
+SUBSTRING(birth_date, 6, 2) AS birth_month
+FROM employee_demographics
+;
+
+SELECT first_name, REPLACE(first_name, 'a', 'z')
+FROM employee_demographics
+;
+
+SELECT first_name, LOCATE('An', first_name)
+FROM employee_demographics
+;
+
+SELECT first_name, last_name,
+CONCAT(first_name, " ", last_name) AS full_name
+FROM employee_demographics
+;
